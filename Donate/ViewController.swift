356cefb917976f0bfe3438f8bb61adfdc00e9da0
christophermayfield <christopher.j.mayfield@gmail.com>
2018-01-01 23:20:30 -0800

    add payment to sender function

--- Donate/ViewController.swift
+++ Donate/ViewController.swift
@@ -13,9 +13,12 @@ import Braintree
 class ViewController: UIViewController {
     
     let toKinizationKey = "REPLACE_THIS_WITH_YOUR_TOKENIZATION_KEY"
+    
+    
 
     @IBOutlet weak var amountTextField: UITextField!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     @IBOutlet weak var donateButton: UIButton! {
         didSet {
             donateButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -42, bottom: 0, right: 0)
@@ -52,5 +55,28 @@ class ViewController: UIViewController {
             self.present(alertController, animated: true, completion: nil)
         }
     }
+    
+    func sendRequestPaymentToServer(nonce: String, amount: String) {
+        let paymentURL = URL(string: "http://localhost/braintree/pay.php")!
+        var request = URLRequest(url: paymentURL)
+        request.httpBody = "payment_method_nonce=\(nonce)&amount=\(amount)".data(using: String.Encoding.utf8)
+        request.httpMethod = "POST"
+        
+        URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) -> Void in
+            guard let data = data else {
+                print(message: error!.localizedDescription)
+                return
+            }
+            
+            guard let result = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any], let success = result?["success"] as? Bool, success == true else {
+                print(message: "Transaction failed. Please try again.")
+                return
+            }
+            
+            print(message: "Successfully charged. Thanks So Much :)")
+            }.resume()
+
+    
+    
 }
 
